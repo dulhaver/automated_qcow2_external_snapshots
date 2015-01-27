e653589abd1a82a95bed2f86022267285f2bd25f
vrms <vrms@netcologne.de>
2015-01-27 18:19:58 +0800

    removed draft content from master

--- qcow2_ext_snapshots.sh
+++ qcow2_ext_snapshots.sh
@@ -1,17 +1,1 @@
-###################### this is a DRAFT ###############
-#
-#
-#   1. virsh shutdown [base_name]
-#   2. cd /path/to/image_location
-#   3. qemu-img create -f qcow2 -b [base_name.qcow2) [new_snapshot.qcow2]
-#   4. cd /etc/libvirt/qemu
-#   5. cp base_name.xml new_snapshot.xml
-#   6. vim new_snapshot.xml
-#   7. .... manually adjust mac address, uuid, image file name, vm name
-#   8. virsh define [new_snapshot.xml]
-#   9. virsh start [new_snapshot]
-
-#!sh
-TODAY = 
-BASE = "
-
+    nothing here yet ! 
